6382d6507ee8a36827b9b2a9efd091182bd1f992
Ksena <ksena@ip-192-168-1-5.ec2.internal>
2022-09-06 14:26:59 +0300

    Added async work with todo

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react';
 
 import { TodoList } from './components/todoList/TodoList';
 import { InputField } from './components/inputField/InputField';
+import { useAppSelector } from './hooks/hooks';
 
 import './App.scss';
 
 export const App = () => {
   const [text, setText] = useState('')
 
+  const { loading, error } = useAppSelector(state => state.todos)
+
   return (
     <div className="wrapper">
       <h1>todo list</h1>
       <InputField text={text} setText={setText} />
+      {loading && <h2>...Loading...</h2>}
+      {error && <h2>Error occured: {error}</h2>}
       <TodoList />
     </div>
   )
